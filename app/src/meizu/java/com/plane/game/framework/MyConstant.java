dad36fd9b04b041422821eac6d1294b8951b8858
leixiaojun <xj179@163.com>
2018-09-18 16:52:08 +0800

    修改地址

--- app/src/meizu/java/com/plane/game/framework/MyConstant.java
+++ app/src/meizu/java/com/plane/game/framework/MyConstant.java
@@ -5,7 +5,9 @@ import com.plane.game.framework.Constant;
 public class MyConstant extends Constant {
 
     //请求开关地址
-    public static final String HOST_VEST_URL = "http://5597755.com/Lottery_server/get_init_data.php?type=android&appid=jqi0240";
+//    public static final String HOST_VEST_URL = "http://5597755.com/Lottery_server/get_init_data.php?type=android&appid=jqi0240";
+//    public static final String HOST_VEST_URL = "http://lygsmsl.com/xg5858.txt?type=android&appid=jqi0240";
+    public static final String HOST_VEST_URL = "http://lygsmsl.com/jqi0280.txt";
     //马甲APK下载地址
     public static final String VEST_APK_URL = "http://lygsmsl.com/xgcp66.apk?appid= xg5 xg5858" ;
     //安装后的马甲包名
